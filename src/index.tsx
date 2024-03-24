3a179f16fd98a6b493873515a02aca5a45e2fbbb
enderNakamoto <ender.nakamoto@gmail.com>
2024-03-24 01:14:56 -0700

    first frame with devtools

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import { Button, Frog } from "@airstack/frog";
+import { devtools } from "@airstack/frog/dev";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { config } from "dotenv";
+
+config();
+
+// Instantiate new Frog instance with Airstack API key
+export const app = new Frog({
+  apiKey: process.env.AIRSTACK_API_KEY as string,
+});
+
+app.frame("/", async (c) => {
+  const { status } = c;
+  return c.res({
+    image: (
+      <div
+        style={{
+          color: "white",
+          display: "flex",
+          fontSize: 40,
+        }}
+      >
+        {status === "initial" ? "Initial Frame" : "Response Frame"}
+      </div>
+    ),
+    intents: [status === "initial" && <Button>Click Here</Button>],
+  });
+});
+
+devtools(app, { serveStatic });
